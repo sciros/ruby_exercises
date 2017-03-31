3e12bfc158536fe393931c3d249187a8e6c55955
Dmitry Sharkov <dsharkov@gmail.com>
2017-03-31 13:17:31 -0400

    removed solutions from master

--- solutions/closest_two_words_solution.rb
+++ solutions/closest_two_words_solution.rb
@@ -12,27 +12,3 @@ require 'watir'
 # (distance measured by the number of words between them).
 #
 # 4. Print that section, capturing both of the words, to the screen.
-
-browser = Watir::Browser.new :chrome
-browser.goto 'http://sciros.net/blog/?p=64'
-text = browser.text
-
-@words = text.split(/\W+/)
-
-def find_indices_of_word(word)
-  @words.each_index.select{|i| @words[i] =~ word}
-end
-
-indices_of_word1 = find_indices_of_word /estimate/
-indices_of_word2 = find_indices_of_word /error/
-
-closest_pairs = []
-
-indices_of_word1.each do |word1_index|
-  closest_word2_index = indices_of_word2.min_by{ |word2_index| (word1_index - word2_index).abs}
-  closest_pairs << [word1_index, closest_word2_index]
-end
-
-closest_pair = closest_pairs.min_by{ |pair| (pair[0] - pair[1]).abs }
-
-puts @words[closest_pair.min..closest_pair.max]
--- solutions/sort_strings_by_cost_solution.rb
+++ solutions/sort_strings_by_cost_solution.rb
@@ -6,19 +6,6 @@ require 'rspec'
 # given an array of strings
 # sort them according to 'cost' ascending
 
-def calculate_character_cost(character)
-  ('a'..'z').find_index(character)+1
-  # or
-  character.ord - 'a'.ord + 1
-end
-
-def calculate_cost(string)
-  string.chars.inject(0) {|sum,char| sum + calculate_character_cost(char)}
-end
-
-def sort_strings_by_cost(strings)
-  strings.sort_by{|string| calculate_cost(string)}
-end
 
 # testing the solution
 describe 'sort strings by cost' do
