e2f9986a56bd5b0ddd671996b833375d75064103
Dmitry Sharkov <dsharkov@gmail.com>
2017-03-31 12:48:42 -0400

    removed solutions

--- solutions/find_fastest_cars_solution.rb
+++ solutions/find_fastest_cars_solution.rb
@@ -11,19 +11,11 @@ cars = YAML.load_file('../data_for_exercises/cars.yml')
 # Given our list of cars, determine the five fastest from 0 to 60 mph.
 # Print their names (make and model) to the screen, in ascending order of their 0-to-60 time.
 
-top_five_zero_to_sixty = cars.sort_by{|car| car.zero_to_sixty}[0..4].map{|car| "#{car.make} #{car.model}"}
-puts 'Top 10 0-to-60:'
-puts top_five_zero_to_sixty
-puts '-----------'
 
 # Challenge 2:
 # Given our list of cars, determine the five with the highest horsepower.
 # Print only the horsepower numbers to the screen, in descending order.
 
-top_five_horsepower = cars.sort_by{|car| car.horsepower}.reverse[0..4].map(&:horsepower)
-puts 'Top 5 horsepower:'
-puts top_five_horsepower
-puts '-----------'
 
 # Challenge 3:
 # The power-to-weight ratio of a car is a good indicator of performance.
@@ -32,13 +24,4 @@ puts '-----------'
 # 1. determine which five cars have the best power-to-weight ratio and print their names
 # 2. of those five cars, print which ones are also in the top five for 0-to-60 time
 
-top_five_power_to_weight = cars.sort_by{ |car| car.weight / car.horsepower }[0..4].map{|car| "#{car.make} #{car.model}"}
-puts 'Top 10 power to weight ratio:'
-puts top_five_power_to_weight
-puts '-------------'
-
-fastest_and_best_ratio = top_five_power_to_weight & top_five_zero_to_sixty
-puts 'Fastest and Best Power to Weight:'
-puts fastest_and_best_ratio
-puts '-------------'
 
--- solutions/sort_strings_by_cost_solution.rb
+++ solutions/sort_strings_by_cost_solution.rb
@@ -6,23 +6,4 @@ require 'rspec'
 # given an array of strings
 # sort them according to 'cost' ascending
 
-def calculate_character_cost(character)
-  ('a'..'z').find_index(character)+1
-  # or
-  character.ord - 'a'.ord + 1
-end
 
-def calculate_cost(string)
-  string.chars.inject(0) {|sum,char| sum + calculate_character_cost(char)}
-end
-
-def sort_strings_by_cost(strings)
-  strings.sort_by{|string| calculate_cost(string)}
-end
-
-# testing the solution
-describe 'sort strings by cost' do
-  it 'should sort strings according to cost' do
-    expect(sort_strings_by_cost(%w(tem cab exz))).to eq(%w(cab tem exz))
-  end
-end
